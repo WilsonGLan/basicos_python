f658f0af3316244562d58beb69cb3086c353df86
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-01-26 23:40:41 -0500

    corrigiendo espacios

--- ayudas.py
+++ ayudas.py
@@ -1,10 +1,10 @@
 # Las funciones min y max ayudan a encontrar el minimo o el maximo de una lista de numeros.
 
-numeroMayor = min(15,11,132,75)
+numeroMayor = min(15, 11, 132, 75)
 
 # imprimir el resultado
 print("El número más grande es:", numeroMayor) 
 
 
-#ensayando la operación de modulo
-print(2000%4)
+# ensayando la operación de modulo
+print(2000 % 4)
