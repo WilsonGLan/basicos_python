d62c227311df3149f508126d4c5842758376ab71
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-02-11 03:15:10 -0500

    Refactorizando metodo suml

--- modulo_prueba_a.py
+++ modulo_prueba_a.py
@@ -4,15 +4,15 @@
 
 __counter = 0
 
-
 def suml(the_list):
     global __counter
     __counter += 1
-    the_sum = 0
-    for element in the_list:
-        the_sum += element
-    return the_sum
-
+    """ Las siguientes lineas se omitieron para refactorizar """
+    #the_sum = 0
+    #for element in the_list:
+    #    the_sum += element
+    #return the_sum
+    return sum(the_list)
 
 def prodl(the_list):
     global __counter    
@@ -21,7 +21,6 @@ def prodl(the_list):
     for element in the_list:
         prod *= element
     return prod
-
 
 if __name__ == "__main__":
     print("I prefer to be a module, but I can do some tests for you.")
