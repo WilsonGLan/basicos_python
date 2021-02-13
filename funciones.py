3ff5bf00996359c83444554a9b72cec60d417418
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-02-12 23:41:46 -0500

    refactorizando linea 172

--- funciones.py
+++ funciones.py
@@ -163,13 +163,13 @@ for i in range(1, 20):
 
 def fib(n):
     if n < 1:
-         return None
+        return None
     if n < 3:
         return 1
 
     elem1 = elem2 = 1
     sum = 0
-    for i in range(3, n + 1):
+    for _ in range(3, n + 1):
         sum = elem1 + elem2
         elem1, elem2 = elem2, sum
     return sum
